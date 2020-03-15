2a035766026950e8a0912e400a78f30b69264373
Dollarsign <lord.cjay14@gmail.com>
2020-03-16 00:19:38 +0800

    Completed 80% of registration part of the curriculum part

--- src/Database/DatabaseOperations.php
+++ src/Database/DatabaseOperations.php
@@ -19,11 +19,34 @@ class DatabaseOperations {
     }
 
     public function getSubjects(){
+        //setting up test_drupal_data database into active connection
+        Database::setActiveConnection('test_drupal_data');
+        // get the active connection and put into an object
+        $connection = Database::getConnection();
+
+        /**
+         * Example Query
+         * $query = $database->query("SELECT id, example FROM {mytable} WHERE created > :created", [
+         *      ':created' => REQUEST_TIME - 3600,
+         *    ]);
+         */
+
+        $query = $connection->query("SELECT * FROM {subjects}");
+
+        $result = $query->fetchAll();
+
+        Database::closeConnection();
+
+        return $result;
+        
+    }
+
+    public function getSubjectsByCollege($collegeUID){
 
         //setting up test_drupal_data database into active connection
         Database::setActiveConnection('test_drupal_data');
         // get the active connection and put into an object
-        $connection = Database::getConnection();;
+        $connection = Database::getConnection();
 
 
         /**
@@ -33,10 +56,20 @@ class DatabaseOperations {
          *    ]);
          */
 
-        $query = $connection->query("SELECT * FROM {programs}");
+        $query = $connection->query("SELECT * FROM {subjects, departments, colleges} WHERE 
+        departments.college_uid = colleges.college_uid
+        AND
+        subjects.department_uid = departments.department_uid
+        AND
+        subjects.subject_isActive = 'active'
+        AND
+        colleges.college_uid = :collegeUID", [
+            ':collegeUID' => $collegeUID
+        ]);
+
         $result = $query->fetchAll();
 
-        Database::setActiveConnection();
+        Database::closeConnection();
 
         return $result;
     }
@@ -77,6 +110,59 @@ class DatabaseOperations {
         */
 
         $query = $connection->query("SELECT * FROM {departments} WHERE college_uid = :collegeUID", 
+        [
+            ':collegeUID' => $collegeUID,
+        ]);
+
+        $result = $query->fetchAll();
+
+        Database::closeConnection();
+
+        return $result;
+    }
+
+    public function getProgramsByDepart($departmentUID){
+
+        //setting up test_drupal_data database into active connection
+        Database::setActiveConnection('test_drupal_data');
+        // get the active connection and put into an object
+        $connection = Database::getConnection();
+
+        /**
+         * Example Query
+        * $query = $database->query("SELECT id, example FROM {mytable} WHERE created > :created", [
+        *      ':created' => REQUEST_TIME - 3600,
+        *    ]);
+        */
+
+        $query = $connection->query("SELECT * FROM {programs} WHERE department_uid = :departmentUID", 
+        [
+            ':departmentUID' => $departmentUID,
+        ]);
+
+        $result = $query->fetchAll();
+
+        Database::closeConnection();
+
+        return $result;
+    }
+
+    public function getProgramsByCollege($collegeUID){
+        //setting up test_drupal_data database into active connection
+        Database::setActiveConnection('test_drupal_data');
+        // get the active connection and put into an object
+        $connection = Database::getConnection();
+
+        /**
+         * Example Query
+        * $query = $database->query("SELECT id, example FROM {mytable} WHERE created > :created", [
+        *      ':created' => REQUEST_TIME - 3600,
+        *    ]);
+        */
+
+        $query = $connection->query("SELECT * FROM {programs, departments, colleges} WHERE 
+        departments.college_uid = colleges.college_uid AND programs.department_uid = departments.department_uid
+        AND colleges.college_uid = :collegeUID", 
         [
             ':collegeUID' => $collegeUID,
         ]);
