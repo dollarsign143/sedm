292f51580a6f0cd064f9fcd5e787c77c51defa86
Dollarsign <lord.cjay14@gmail.com>
2020-03-21 00:57:18 +0800

    Subject adding part is done

--- src/Form/Modals/VerifySubjectModalForm.php
+++ src/Form/Modals/VerifySubjectModalForm.php
@@ -11,6 +11,8 @@ use Drupal\Core\Ajax\OpenDialogCommand;
 use Drupal\Component\Render\FormattableMarkup;
 use Drupal\Core\Url;
 
+use Drupal\sedm\Database\DatabaseOperations;
+
 class VerifySubjectModalForm extends FormBase {
 
   /**
@@ -23,8 +25,9 @@ class VerifySubjectModalForm extends FormBase {
     /**
    * {@inheritdoc}
    */
-  public function buildForm(array $form, FormStateInterface $form_state) {
+  public function buildForm(array $form, FormStateInterface $form_state, $subject = array()) {
 
+    
     $form['#tree'] = TRUE;
     $form['modal-form-container'] = [
         '#type' => 'container',
@@ -32,21 +35,112 @@ class VerifySubjectModalForm extends FormBase {
         '#suffix' => '</div>',
     ]; 
 
-    $form['modal-form-container']['sample'] = [
-      '#type' => 'item',
-      '#markup' => $this->t('I hope this will work!'),
+    $form['modal-form-container']['subject-info-fieldset'] = [
+      '#type' => 'fieldset',
     ];
+
+    $form['modal-form-container']['subject-info-fieldset']['question'] = [
+      '#type' => 'item',
+      '#markup' => $this->t('Are you sure you want to add this subject?'),
+    ];
+
+    $form['modal-form-container']['subject-info-fieldset']['subject-info-container'] = [
+      '#type' => 'container',
+      '#attributes' => [
+        'class' => ['inline-container-col4'],
+      ],
+    ];
+// ++++++++++++++++++++++++++++++++++++++++++++++
+    // Subject Code
+    $form['modal-form-container']['subject-info-fieldset']['subject-info-container']['subject_code'] = [
+      '#type' => 'textfield',
+      '#title' => $this->t('Subject Code'),
+      '#default_value' => $subject['code'],
+      '#disabled' => TRUE,
+      '#attributes' => [
+        'class' => ['flat-input',],
+      ],
+    ];
+    // Subject Description
+    $form['modal-form-container']['subject-info-fieldset']['subject-info-container']['subject-desc'] = [
+      '#type' => 'textfield',
+      '#title' => $this->t('Subject Description'),
+      '#default_value' => $subject['description'],
+      '#disabled' => TRUE,
+      '#attributes' => [
+        'class' => ['flat-input',],
+      ],
+    ];
+// ++++++++++++++++++++++++++++++++++++++++++++++
+// ----------------------------------------------------------------------------
+// ++++++++++++++++++++++++++++++++++++++++++++++
+    // Subject Units
+    $form['modal-form-container']['subject-info-fieldset']['subject-info-container']['subject-units'] = [
+      '#type' => 'textfield',
+      '#title' => $this->t('Subject Units'),
+      '#default_value' => $subject['units'],
+      '#disabled' => TRUE,
+      '#attributes' => [
+        'class' => ['flat-input',],
+      ],
+    ];
+// +++++++++++++++++++++++++++++++++++++++++++++++
+// -----------------------------------------------------------------------------
+// +++++++++++++++++++++++++++++++++++++++++++++++
+    // Subject Lecture Hours
+    $form['modal-form-container']['subject-info-fieldset']['subject-info-container']['subject-lect-hours'] = [
+      '#type' => 'textfield',
+      '#title' => $this->t('Lecture Hours'),
+      '#default_value' => $subject['lecHours'],
+      '#disabled' => TRUE,
+      '#attributes' => [
+        'class' => ['flat-input',],
+      ],
+    ];
+    // Subject Lab Hours
+    $form['modal-form-container']['subject-info-fieldset']['subject-info-container']['subject-lab-hours'] = [
+      '#type' => 'textfield',
+      '#title' => $this->t('Laboratory Hours'),
+      '#default_value' => $subject['labHours'],
+      '#disabled' => TRUE,
+      '#attributes' => [
+        'class' => ['flat-input',],
+      ],
+    ];
+// ++++++++++++++++++++++++++++++++++++++++++++++++
+// -----------------------------------------------------------------------------
+    // Subject is elective
+    $form['modal-form-container']['subject-info-fieldset']['subject-info-container']['subject-is-elective'] = [
+      '#type' => 'textfield',
+      '#title' => $this->t('Subject is Elective?'),
+      '#default_value' => $subject['isElective'] ? 'elective' : 'No',
+      '#disabled' => TRUE,
+      '#attributes' => [
+        'class' => ['flat-input',],
+      ],
+    ];
+    // Subject is active
+    $form['modal-form-container']['subject-info-fieldset']['subject-info-container']['subject-is-active'] = [
+      '#type' => 'textfield',
+      '#title' => $this->t('Subject is Active?'),
+      '#default_value' => $subject['isActive'] ? 'active' : 'No',
+      '#disabled' => TRUE,
+      '#attributes' => [
+        'class' => ['flat-input',],
+      ],
+    ];
+
 
     $form['modal-form-container']['action'] = [
         '#type' => 'action',
     ];
 
-    $subject = 'test';
+    $form_state->set('subject', $subject);
 
     $form['modal-form-container']['action']['submit-yes'] = [
       '#type' => 'button',
       '#value' => $this->t('Yes'),
-      '#data' => $subject,
+      '#data' => ['subject' => $subject],
       '#attributes' => [
         'class' => ['use-ajax',],
       ],
@@ -82,10 +176,22 @@ class VerifySubjectModalForm extends FormBase {
     $response = new AjaxResponse();
     $command = new CloseModalDialogCommand();
     $response->addCommand($command);
-    $content['message'] = [
-      '#type' => 'item',
-      '#markup' => $this->t('Subject has been added successfully!'),
-    ];
+
+    $dbOperations = new DatabaseOperations(); // instantiate DatabaseOperations Class
+    $subj_info = $_SESSION['sedm']['subject'];
+    $result = $dbOperations->addNewSubject($subj_info);
+    if($result){
+      $content['message'] = [
+        '#type' => 'item',
+        '#markup' => $this->t('Subject has been added successfully!'),
+      ];
+    }
+    else {
+      $content['message'] = [
+        '#type' => 'item',
+        '#markup' => $this->t('ERROR! Failed to add the subjec. Please check the error logs!'),
+      ];
+    }
     $command = new OpenDialogCommand('#success-adding-subject', $this->t('Successful'), $content, ['width' => '50%']);
     $response->addCommand($command);
     return $response;
